7a1e03eb7f66a48126cce80b938a498ff7011944
Храмов Алексей Игоревич <out_khramov.ai@pecom.ru>
2021-04-28 13:11:19 +0300

    Change baseURL

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from 'axios'
 import { ResTestsType } from './types'
 
 const instance = axios.create({
-  baseURL: process.env.APP_API_URL || 'https://opentdb.com/',
+  baseURL: 'https://opentdb.com/',
   withCredentials: false
 })
 
